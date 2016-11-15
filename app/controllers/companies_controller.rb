48c5caa0521c66bae59a51571e4687adc3dd5f87
Rodrigo <rcecosta@gmail.com>
2016-11-15 03:44:00 -0200

    Usuário cadastra empresas

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,22 @@
+class CompaniesController < ApplicationController
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company
+    end
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :location, :mail, :phone)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :jobs, only: [:create, :show, :new, :edit, :update]
-  resources :companies, only: [:create, :show, :new, :edit]
+  resources :companies, only: [:create, :show, :new]   
 end
